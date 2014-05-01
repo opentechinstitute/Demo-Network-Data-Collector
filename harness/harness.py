7b98b66147d8237839b2aa201c69541d8c8d3088
Seamus Tuohy <s2e@opentechinstitute.org>
2014-05-01 13:15:33 -0400

    removing ssl
    Since this is simply for test and demo networks and should NEVER be used for active networks we have removed SSL. We are relying on the mesh networks existing mesh level encryption to provide the low level of security desired.

--- harness/harness.py
+++ harness/harness.py
@@ -15,23 +15,12 @@ class HarnessServer(TCPServer):
     def __init__(self,
                  server_address,
                  RequestHandlerClass,
-                 certfile,
-                 keyfile,
-                 ssl_version=ssl.PROTOCOL_TLSv1,
                  bind_and_activate=True):
         TCPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate)
-        self.certfile = certfile
-        self.keyfile = keyfile
-        self.ssl_version = ssl_version
 
     def get_request(self):
         newsocket, fromaddr = self.socket.accept()
-        connstream = ssl.wrap_socket(newsocket,
-                                 server_side=True,
-                                 certfile = self.certfile,
-                                 keyfile = self.keyfile,
-                                 ssl_version = self.ssl_version)
-        return connstream, fromaddr
+        return fromaddr
 
 class MySSL_ThreadingTCPServer(ThreadingMixIn, HarnessServer): pass
 
@@ -40,6 +29,5 @@ class testHandler(StreamRequestHandler):
         data = self.connection.recv(4096)
         self.wfile.write(data)
 
-
 if __name__ == "__main__":
-    MySSL_ThreadingTCPServer(('',5151),testHandler,"temp/cert.pem","temp/key.pem").serve_forever()
+    MySSL_ThreadingTCPServer(('',5151),testHandler).serve_forever()
